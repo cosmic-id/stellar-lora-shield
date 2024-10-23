4e84b9f188bb0e89d882f018d83f4e447656683b
cosmic-id <cosmic.indonesia01@gmail.com>
2024-10-23 17:17:00 +0700

    lora p2p

---
+++ lora-p2p-receiver/lora-p2p-receiver.ino
@@ -0,0 +1,55 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://github.com/cosmic-id/
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+// LoRa <-> Arduino Interfaces
+#define LORA_ARDUINO_NSS  10
+#define LORA_ARDUINO_RST  9
+#define LORA_ARDUINO_DIO0 2
+#define LORA_ARDUINO_DIO1 6
+
+void setup() {  
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(LORA_ARDUINO_NSS, LORA_ARDUINO_RST, LORA_ARDUINO_DIO0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //Indonesia 920.0 - 923.0 MHz 
+  while (!LoRa.begin(920E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+  
+  // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
---
+++ lora-p2p-transmitter/lora-p2p-transmitter.ino
@@ -0,0 +1,53 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://github.com/cosmic-id/
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+// LoRa <-> Arduino Interfaces
+#define LORA_ARDUINO_NSS  10
+#define LORA_ARDUINO_RST  9
+#define LORA_ARDUINO_DIO0 2
+#define LORA_ARDUINO_DIO1 6
+
+int counter = 0;
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(LORA_ARDUINO_NSS, LORA_ARDUINO_RST, LORA_ARDUINO_DIO0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency
+  //Indonesia 920.0 - 923.0 MHz 
+  while (!LoRa.begin(920E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+  
+  // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(10000);
+}
